963dc5223b5b8cbd3f93e3fd641506fda4295678
Terranie Sims <terranie67@gmail.com>
2020-09-13 20:41:44 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,10 +1,11 @@
 def count_strings(array)
   array.count do |element|
     element.class == String
-    
+  end 
 end
 
 def count_empty_strings(array)
   array.count do |element|
     element.empty? 
+  end 
 end
